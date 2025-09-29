b833cef0eade5929e91aa42b565b705f2da3d82a
Rajawali-caffe <rajawalistations9@gmail.com>
2025-09-29 21:11:36 +0700

    Create Customer.jsx

---
+++ Customer.jsx
@@ -0,0 +1,11 @@
+export default function Customer() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
+      <h1>Rajawali Caffe - Menu</h1>
+      <p>Selamat datang! Silakan pilih menu favorit Anda.</p>
+
+      {/* Render daftar menu di sini, sama seperti di App.jsx */}
+      {/* Contoh: <MenuList /> */}
+    </div>
+  );
+}
